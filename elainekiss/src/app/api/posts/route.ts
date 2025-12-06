c62b4598d378c8b50cdc96dfdf8d803518dc1d34
ArtHirche <hirche.arthur@gmail.com>
2025-12-06 14:00:01 -0300

    feat: setup database

---
+++ elainekiss/src/app/api/posts/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server';
+import prisma from '@/lib/prisma';
+
+export async function GET() {
+  const posts = await prisma.post.findMany({
+    include: { author: true },
+    orderBy: { createdAt: 'desc' }
+  });
+
+  return NextResponse.json(posts);
+}
