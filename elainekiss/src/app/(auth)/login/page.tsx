15ab92ec9b8e0a3e86f2d1019b00e6ee43d57eb7
Erik David <102744887+ErikDavidLeitao@users.noreply.github.com>
2026-03-17 16:13:12 -0300

    tela de login estruturada e estilizada

---
+++ elainekiss/src/app/(auth)/login/page.tsx
@@ -0,0 +1,92 @@
+/*
+import styles from './login.module.css';
+
+export default function Login() {
+  return (
+    <main>
+      <div className={styles.loginContainer}>
+        <h1>Login</h1>
+
+        <form className={styles.loginForm}>
+          <div className={styles.inputGroup}>
+            <p>Login:</p>
+            <input type="email" placeholder="Email" />
+          </div>
+
+          <div className={styles.inputGroup}>
+            <p>Senha:</p>
+            <input type="password" placeholder="Senha" />
+          </div>
+
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+
+    </main>
+  );
+}
+*/
+
+import styles from "./login.module.css";
+
+export default function Login() {
+  return (
+    <main className={styles.container}>
+
+      <div className={styles.loginContainer}>
+        <div className={styles.formBox}>
+
+          <h1 className={styles.title}>Acesse sua conta</h1>
+
+          <form className={styles.form}>
+
+            <label>Email</label>
+            <input
+              type="email"
+              placeholder="nome@exemplo.com"
+              className={styles.input}
+            />
+
+            <label>Senha</label>
+            <input
+              type="password"
+              placeholder="Digite sua senha"
+              className={styles.input}
+            />
+
+            <a href="#" className={styles.forgot}>
+              Esqueci minha senha
+            </a>
+
+            <button className={styles.loginButton}>
+              Acessar minha loja
+            </button>
+
+            <div className={styles.separator}>
+              <span></span>
+              <p>ou</p>
+              <span></span>
+            </div>
+
+            <div className={styles.socialButtons}>
+              <button className={styles.google}>
+                Acessar com Google
+              </button>
+
+              <button className={styles.apple}>
+                Acessar com Apple
+              </button>
+            </div>
+
+          </form>
+
+          <p className={styles.create}>
+            Ainda não tem uma loja? <a href="#">Criar agora</a>
+          </p>
+
+        </div>
+      </div>
+
+    </main>
+  );
+}
